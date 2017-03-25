78c80271590da26aed9f071c1f4139fd1263bb11
Jeremy Kun <j2kun@users.noreply.github.com>
2017-03-25 15:09:11 -0700

    js: create demo hyperplane separator

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
-import { Point } from './point';
+import { Point, Hyperplane, innerProduct } from './geometry';
 
 
-var positivePoints = [
-  [10, 30],
-  [20, 10],
-  [90, 60],
-];
-
-var negativePoints = [
-  [-15, 25],
-  [-20, -10],
-  [-90, -60],
-];
+let canvas = document.getElementById('myCanvas');
+let context = canvas.getContext('2d');
+let originX = canvas.width / 2;
+let originY = canvas.height / 2;
 
 
+var h = new Hyperplane(20, 30, 0);
+h.render();
+
+var numPoints = 30;
 var data = [];
 
-for (let coords of positivePoints) {
-  data.push(new Point(coords[0], coords[1], 1));
-}
+for (let i=0; i < numPoints; i++) {
+  let unlabeledPoint = new Point(0, 0);
+  while (Math.abs(innerProduct(unlabeledPoint, h.normalized())) < 40) {
+    unlabeledPoint = new Point(
+      Math.floor(Math.random() * canvas.width - originX),
+      Math.floor(Math.random() * canvas.height - originY)
+    );
+  }
 
-for (let coords of negativePoints) {
-  data.push(new Point(coords[0], coords[1], -1));
+  if (innerProduct(unlabeledPoint, h) > 0) {
+    unlabeledPoint.label = 1;
+  } else {
+    unlabeledPoint.label = -1;
+  }
+
+  data.push(unlabeledPoint);
 }
 
 for (let point of data) {
