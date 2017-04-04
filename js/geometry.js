c9d138d5f80f58670b7ea309ccbadd0da2e85eea
Jeremy Kun <kun.jeremy@gmail.com>
2017-04-03 22:23:48 -0700

    js: switch to d3

--- js/geometry.js
+++ js/geometry.js
@@ -4,8 +4,7 @@ function innerProduct(a, b) {
 
 
 class Point {
-
-  constructor(x, y, label) {
+  constructor(x, y, label=1) {
     this.x = x;
     this.y = y;
     this.label = label ? label : 1;  // labels are +/-1
@@ -21,22 +20,6 @@ class Point {
     };
   }
 
-  render() {
-    let canvas = document.getElementById('myCanvas');
-    let context = canvas.getContext('2d');
-    let centerX = canvas.width / 2 + this.x;
-    let centerY = canvas.height / 2 - this.y;
-
-    context.beginPath();
-    context.arc(centerX, centerY, this.radius, 0, 2 * Math.PI, false);
-    context.fillStyle = this.labelToColor[this.label];
-    context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = this.labelToStrokeColor[this.label];
-    context.stroke();
-    context.closePath();
-  }
-
   toString() {
     return `(${this.x}, ${this.y}, ${this.label > 0 ? '+': '-'}1)`;
   }
@@ -44,7 +27,6 @@ class Point {
 
 
 class Hyperplane {
-
   constructor(normalX=0, normalY=0, offset=0) {
     // hyperplane is defined by all vectors x for which <normal, x> - b = 0
     // either normalX or normalY must be nonzero
@@ -63,6 +45,10 @@ class Hyperplane {
     return new Point(this.x / norm, this.y / norm);
   }
 
+  normalVector() {
+    return new Point(this.x, this.y);
+  }
+
   spanningVector() {
     // since this demo is in 2d, the hyerplane is just a line. spanningVector
     // outputs a unit vector v such that the line is the set { c*v + this.b } for
@@ -78,46 +64,39 @@ class Hyperplane {
       spanningY = -this.x / this.y;
     }
     let norm = Math.sqrt(spanningX * spanningX + spanningY * spanningY);
-    let vec = [spanningX / norm, spanningY / norm];
-    console.log(`Spanning vector: (${vec[0]}, ${vec[1]})`);
-    return vec;
+    return new Point(spanningX / norm, spanningY / norm);
   }
 
-  arrow(context, fromX, fromY, toX, toY){
-    var headLen = 10;   // length of head in pixels
-    var angle = Math.atan2(toY - fromY, toX - fromX);
-    context.beginPath();
-    context.lineWidth = 2;
-    context.moveTo(fromX, fromY);
-    context.lineTo(toX, toY);
-    context.lineTo(toX - headLen * Math.cos(angle - Math.PI/6), toY - headLen * Math.sin(angle - Math.PI/6));
-    context.moveTo(toX, toY);
-    context.lineTo(toX - headLen * Math.cos(angle + Math.PI/6), toY - headLen * Math.sin(angle + Math.PI/6));
-    context.stroke();
-    context.closePath();
-  }
-
-  render() {
-    let canvas = document.getElementById('myCanvas');
-    let context = canvas.getContext('2d');
-    let originX = canvas.width / 2;
-    let originY = canvas.height / 2;
+  spanningX1(width, height) {
+    // helper for d3
     let direction = this.spanningVector();
-    let magnitude = canvas.width + canvas.height;  // ensure line covers canvas (overkill, but fine)
+    let magnitude = width + height;  // ensure line covers canvas (overkill, but fine)
 
-    let startX = parseInt(originX + magnitude * direction[0]);
-    let startY = parseInt(originY - magnitude * direction[1]);
-    let endX = parseInt(originX - magnitude * direction[0]);
-    let endY = parseInt(originY + magnitude * direction[1]);
+    return parseInt(magnitude * direction.x);
+  }
 
-    context.beginPath();
-    context.lineWidth = 2;
-    context.moveTo(startX, startY);
-    context.lineTo(endX, endY);
-    context.stroke();
-    context.closePath();
+  spanningX2(width, height) {
+    // helper for d3
+    let direction = this.spanningVector();
+    let magnitude = width + height;  // ensure line covers canvas (overkill, but fine)
 
-    this.arrow(context, originX, originY, originX + this.x, originY - this.y);
+    return parseInt(-magnitude * direction.x);
+  }
+
+  spanningY1(width, height) {
+    // helper for d3
+    let direction = this.spanningVector();
+    let magnitude = width + height;  // ensure line covers canvas (overkill, but fine)
+
+    return parseInt(magnitude * direction.y);
+  }
+
+  spanningY2(width, height) {
+    // helper for d3
+    let direction = this.spanningVector();
+    let magnitude = width + height;  // ensure line covers canvas (overkill, but fine)
+
+    return parseInt(-magnitude * direction.y);
   }
 }
 
